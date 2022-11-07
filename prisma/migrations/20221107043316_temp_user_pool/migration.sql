4fce7d1849ddf75e5a07a35d390994a27e081db2
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-06 21:33:33 -0700

    fix migration mistake

--- prisma/migrations/20221107043316_temp_user_pool/migration.sql
+++ prisma/migrations/20221107043316_temp_user_pool/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "unregisteredSquareUser" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "emailHash" TEXT NOT NULL,
     "squareCustomerId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
