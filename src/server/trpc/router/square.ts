9465488aa8458672b394ede558f2b47ff0edf4e3
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-07 00:14:28 -0700

    GL-42| add SQ customer + start Subscription

--- src/server/trpc/router/square.ts
+++ src/server/trpc/router/square.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import { Prisma } from '@prisma/client'
 import { TRPCError } from '@trpc/server'
 import * as bcrypt from 'bcrypt'
 import { z } from 'zod'
@@ -7,6 +9,8 @@ import { getSquareServices } from '~/server/api/square/getServices'
 import { getSquareSubscriptions } from '~/server/api/square/getSubscriptions'
 import { squareClient } from '~/server/common/squareClient'
 import { router, publicProcedure } from '~/server/trpc/trpc'
+
+const HASH_ROUNDS = 12
 
 export const squareRouter = router({
 	getServices: publicProcedure.query(async () => {
@@ -33,68 +37,108 @@ export const squareRouter = router({
 			})
 		}
 	}),
-	initiateNewCustomerSubscription: publicProcedure
+	createNewSquareCustomer: publicProcedure
 		.input(
 			z
 				.object({
-					planId: z.string(),
-					firstName: z.string().optional(),
-					lastName: z.string().optional(),
-					email: z.string().optional(),
-					userId: z.string().optional(),
+					firstName: z.string(),
+					lastName: z.string(),
+					email: z.string(),
 				})
 				.required(),
 		)
 		.mutation(async ({ ctx, input }) => {
+			try {
+				// find user
+				const isLoggedIn = !!ctx.session?.user?.id
+				const searchParams: Prisma.UserFindFirstArgs = isLoggedIn
+					? { where: { id: ctx.session?.user?.id } }
+					: { where: { email: input.email } }
+				const dbUser = await ctx.prisma.user.findFirst(searchParams)
+
+				// check if user and squareCustomerId
+				if (dbUser && dbUser?.squareCustomerId) {
+					return dbUser?.squareCustomerId
+				}
+				const tempUser = await ctx.prisma.unregisteredSquareUser.findFirst({
+					where: { emailHash: await bcrypt.hash(input.email, HASH_ROUNDS) },
+				})
+				if (tempUser) {
+					return tempUser.squareCustomerId
+				}
+
+				// no dbUser w/ NO customerId || !tempUser, so we create one
+				const {
+					result: { customer },
+				} = await squareClient.customersApi.createCustomer({
+					givenName: input.firstName,
+					familyName: input.lastName,
+					emailAddress: input.email,
+				})
+
+				const squareCustomerId = customer?.id
+				if (!squareCustomerId) {
+					throw new Error('ERROR creating new square customer')
+				}
+				console.log('created square user :: %s', squareCustomerId)
+
+				if (dbUser) {
+					// user acc found but no squareInfo, then update
+					await ctx.prisma.user.update({
+						where: { id: dbUser.id },
+						data: { squareCustomerId },
+					})
+					console.log('Updated User')
+				} else if (!tempUser) {
+					// this is not a registration flow
+					// create tempUser in db in case they want to register later
+					const newTempUser = await ctx.prisma.unregisteredSquareUser.create({
+						data: {
+							emailHash: await bcrypt.hash(input.email, HASH_ROUNDS),
+							squareCustomerId,
+						},
+					})
+					console.log('created temp ::', newTempUser)
+				}
+
+				return squareCustomerId
+			} catch (error) {
+				throw new TRPCError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: 'An unexpected error occurred, please try again later.',
+					cause: error,
+				})
+			}
+		}),
+	initiateCustomerSubscription: publicProcedure
+		.input(
+			z
+				.object({
+					planId: z.string(),
+					customerId: z.string(),
+				})
+				.required(),
+		)
+		.mutation(async ({ input }) => {
 			// need UUID for square API
 			try {
 				const idempotencyKey = crypto.randomUUID()
 				// need locationId for square API
 				const locationId = env.SQUARE_LOCATION_ID
 
-				// need square customerId for square API
-				const userId = ctx.session?.user?.id ?? input.userId
-				console.log('fetching user with:: %s', userId)
-				const user = await ctx.prisma.user.findFirst({
-					where: { id: userId },
-				})
-				let squareCustomerId: string
-				// if authenticated, check user for square customerId
-				if (user) {
-					console.log(
-						'User :: {%s} found with SquareId :: %s',
-						user?.id,
-						user?.squareCustomerId,
-					)
-					squareCustomerId = user!.squareCustomerId!
-				} else {
-					// we create a customer using F/L name + email
-					const { result } = await squareClient.customersApi.createCustomer({
-						givenName: input.firstName,
-						familyName: input.lastName,
-						emailAddress: input.email,
-					})
-					squareCustomerId = result!.customer!.id as string
-					console.log('created square user :: %s', squareCustomerId)
-					// create tempUser in db in case they want to register later
-					await ctx.prisma.unregisteredSquareUser.create({
-						data: {
-							emailHash: await bcrypt.hash(input.email, 8),
-							squareCustomerId,
-						},
-					})
-				}
 				// create subscription using customerInfo
-				const { result: newSubscriptionRes } =
-					await squareClient.subscriptionsApi.createSubscription({
-						idempotencyKey,
-						locationId,
-						planId: input.planId,
-						customerId: squareCustomerId,
-					})
+				const {
+					result: { subscription },
+				} = await squareClient.subscriptionsApi.createSubscription({
+					idempotencyKey,
+					locationId,
+					planId: input.planId,
+					customerId: input.customerId,
+				})
 
-				console.log('Created subscription:: ', newSubscriptionRes?.subscription)
-				return newSubscriptionRes?.subscription
+				console.log('Created subscription:: ', subscription)
+				// square will send an invoice to the customer's email for payment
+				return subscription
 			} catch (error) {
 				throw new TRPCError({
 					code: 'INTERNAL_SERVER_ERROR',
@@ -104,6 +148,3 @@ export const squareRouter = router({
 			}
 		}),
 })
-
-// SUB PLAN :: COMFORT :: JK3HCPWTUUHWVWPQFTODR7GC
-// Customer :: marK@test.com :: NHHC96B7T2RDBJP3WN3KH0EMPG
