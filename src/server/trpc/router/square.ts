90eeedbc03518bd4b46a6b328508f1b8c5f9c75f
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-07 22:58:53 -0700

    GL-44| add tRPC func

--- src/server/trpc/router/square.ts
+++ src/server/trpc/router/square.ts
@@ -1,4 +1,6 @@
+import { NewsletterSignUps, Prisma } from '@prisma/client'
 import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
 
 import { getSquareServices } from '~/server/api/square/getServices'
 import { getSquareSubscriptions } from '~/server/api/square/getSubscriptions'
@@ -29,4 +31,29 @@ export const squareRouter = router({
 			})
 		}
 	}),
+	addEmailToNewsletter: publicProcedure
+		.input(
+			z
+				.object({
+					email: z.string().email(),
+				})
+				.required(),
+		)
+		.mutation(async ({ ctx, input }) => {
+			try {
+				const newsletterSignUp: NewsletterSignUps =
+					await ctx.prisma.newsletterSignUps.create({
+						data: {
+							email: input.email,
+						},
+					})
+				return newsletterSignUp?.id
+			} catch (error) {
+				throw new TRPCError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: 'An unexpected error occurred, please try again later.',
+					cause: error,
+				})
+			}
+		}),
 })
