ef451990ba6dd098021a4fe810b32e6eb715d345
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-11-29 12:31:20 -0700

    GL-9| Services Banner (#9)
    * GL-9| install fonts
    
    * GL-9| fix prev header theme defaults
    
    * GL-9| complete work
    
    * GL-9| fix padding issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,13 @@ module.exports = {
 					headings: '#405861',
 					textPrimary: '#85a3b3',
 					textSecondary: '#879296',
+					textCaptions: '#A1B8C4',
+					khaki: '#F7EFE8',
 				},
 			},
+		},
+		fontFamily: {
+			serif: ['Cormorant Garamond', 'serif'],
 		},
 	},
 	plugins: [],
